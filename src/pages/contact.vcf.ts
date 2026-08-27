45717c7055e3b094597d31500e69df9d8482155f
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2026-08-27 14:42:54 -0400

    Add the contact paths: card to show, vCard, prefilled emails

---
+++ src/pages/contact.vcf.ts
@@ -0,0 +1,30 @@
+import type { APIRoute } from "astro";
+import { site } from "../data/site";
+
+// Un tap, et Mariane est dans le répertoire. Généré au build, servi en statique.
+export const GET: APIRoute = ({ site: origine }) => {
+  const [prenom, ...restePrenom] = site.nom.split(" ");
+  const nomFamille = restePrenom.join(" ");
+
+  const carte = [
+    "BEGIN:VCARD",
+    "VERSION:3.0",
+    `N:${nomFamille};${prenom};;;`,
+    `FN:${site.nom}`,
+    `TITLE:${site.metier}`,
+    `EMAIL;TYPE=INTERNET:${site.courriel}`,
+    `TEL;TYPE=CELL:${site.telephone}`,
+    origine ? `URL:${origine.href}` : "",
+    `NOTE:${site.accroche}`,
+    "END:VCARD",
+  ]
+    .filter(Boolean)
+    .join("\r\n");
+
+  return new Response(carte, {
+    headers: {
+      "Content-Type": "text/vcard; charset=utf-8",
+      "Content-Disposition": 'attachment; filename="mariane-rollot-carlo.vcf"',
+    },
+  });
+};
